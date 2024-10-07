9cf7dbd5020fc4a051f08bc121e468d4b7b86f73
Chhavi <109955571+chhavipal01@users.noreply.github.com>
2024-10-07 18:26:21 +0530

    Time: 1 ms (21.94%), Space: 41.1 MB (89.88%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,13 +1,26 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length())
-        {
-            return false; 
-        }
+      if(s.length() != goal.length()){
+        return false;
+      }
+      
+      s = s+s;
+      for(int i =0 ;i<=s.length()-goal.length() ; i++)
+      {
+        int j;
+        for(j = 0 ; j<goal.length() ; j++){
+            if (s.charAt(i + j) != goal.charAt(j)) {
+               break;  
+                }
+            }
+            if (j == goal.length()) {
+                return true;  
+            }
+        
+      
+      }
+      return false;
 
-        String con = " ";
-        con = s+s;
-         return con.contains(goal);
+        
     }
-
 }
